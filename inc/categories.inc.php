3ce99f626011ce7766ff32985934858ba29a283b
Aswin K O <aswinko479@gmail.com>
2022-10-31 21:54:42 +0530

    implement neumorphism in category section in home page. and changes some styles in category section.

--- inc/categories.inc.php
+++ inc/categories.inc.php
@@ -10,20 +10,19 @@
 
 ?>
 
-<section class="category my-5 bg-dark">
+<section class="category my-5" style="background: #202124;">
     <div class="container mb-5">
         <div class="row py-4">
             <div class="col-12">
-                <h2 class="category_title text-light fw-bold">Top Categories</h2>
+                <h2 class="category_title fw-bold">Top Categories</h2>
             </div>
         </div>
         <?php if($category): ?>
             <div class="row text-center d-flex flex-wrap">
                 <?php foreach($category as $categories): ?>
-                    <div class="col-3 mb-4">
-                        <a class="text-dark" href="category?category_id=<?php echo htmlspecialchars($categories['category_id']); ?>&category_name=<?php echo htmlspecialchars($categories['category_name']); ?>">
-                        <?php //echo htmlspecialchars($categories['category_name']); ?>
-                            <div class="card">
+                    <div class="col-3 col-xl-3 col-md-4 col-sm-6 mb-3">
+                        <a class="" href="category?category_id=<?php echo htmlspecialchars($categories['category_id']); ?>&category_name=<?php echo htmlspecialchars($categories['category_name']); ?>">
+                            <div class="category-card card border border-4 shadow-lg">
                                 <div class="card-body category-focus">
                                     <i class="<?php echo htmlspecialchars($categories['category_icon']); ?>"></i>
                                     <h5 class="card-title"><?php echo htmlspecialchars($categories['category_name']); ?></h5>                    
